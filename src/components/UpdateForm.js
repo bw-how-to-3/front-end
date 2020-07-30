4fa01085904b7b301c86117b46d520d396aed87c
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-07-30 19:20:48 -0400

    pushing thursdays final code

--- src/components/UpdateForm.js
+++ src/components/UpdateForm.js
@@ -1,81 +1,61 @@
-import React, { useContext, useState, useEffect } from 'react'
-import { useHistory, useParams, useLocation } from 'react-router-dom'
-import axiosWithAuth from '../utils/axiosWithAuth'
-import { HowToContext } from '../contexts/HowToContext'
-
+import React, { useContext, useState, useEffect } from "react";
+import { useHistory, useParams, useLocation } from "react-router-dom";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import { HowToContext } from "../contexts/HowToContext";
 
 const UpdateForm = (props) => {
-const { skills, setSkills } = useContext(HowToContext)
-const { id } = useParams()
-const location = useLocation();
-const { push } = useHistory()
+  const { id } = useParams();
+  const { push } = useHistory();
 
-const [skill, setSkill] = useState ({
-    title: '',
-    body:'',
-})
-// useEffect(() => {
-//     if(location.state) {
-//         setSkill(location.state)
-//     }else{
-//     axiosWithAuth()
-//     .get(`/posts/posts/${id}`, skill)
-//     .then(res => {
-//         setSkill(res.data)
-        
-//     })
-//     .catch(error => {
-//         console.log(error)
-//     })
-//   }  
-// }, [id])
+  const [skill, setSkill] = useState({
+    title: "",
+    body: "",
+  });
 
-const handleChanges = (e) => {
+  const handleChanges = (e) => {
     e.persist();
     setSkill({
-        ...skill,
-        [e.target.name]: e.target.value
-    })
-}
+      ...skill,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(id)
+    console.log(id);
     axiosWithAuth()
-    .put(`/posts/post/${id}`, skill)
-    .then(res => {
-        console.log(res)
-        // setSkills(res.data)
-        push(`/skills-list/`)
-    })
-    .catch(error => {
-        console.log(error)
-    })
-}
-    return (
-        <div>
-            <h2>Update your How-To</h2>
-            <form onSubmit={handleSubmit}>
-                <input
-                type='text'
-                name='title'
-                onChange={handleChanges}
-                value={skill.title}
-                placeholder='update title'
-                /><br/>
-                <textarea
-                name='body'
-                value={skill.body}
-                onChange={handleChanges}
-                placeholder='update description'
-                /><br />
-                <button>Update</button>
-            </form>
-    
-        </div>
-    )
+      .put(`/posts/post/${id}`, skill)
+      .then((res) => {
+        console.log(res);
+        push(`/skills-list/`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <h2>Update your How-To</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          onChange={handleChanges}
+          value={skill.title}
+          placeholder="update title"
+        />
+        <br />
+        <textarea
+          name="body"
+          value={skill.body}
+          onChange={handleChanges}
+          placeholder="update description"
+        />
+        <br />
+        <button>Update</button>
+      </form>
+    </div>
+  );
+};
 
-
-}
-
-export default UpdateForm
+export default UpdateForm;
